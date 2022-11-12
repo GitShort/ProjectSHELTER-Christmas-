c30138378e983aea09892aaed56aaeb19913be24
Airidas <airidas12111@gmail.com>
2022-11-12 20:54:47 +0200

    Movement, shooting

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] GameObject projectileExplosion;
+    [SerializeField] float VFXOffset = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject explosion = Instantiate(projectileExplosion, transform.position - transform.position * VFXOffset, transform.rotation);
+        explosion.GetComponent<VisualEffect>().Play();
+        //AudioManager.instance.PlaySound("ProjectileExplode", explosion);
+        Destroy(explosion, 3f);
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/TopDownCharacterMover.cs
+++ Assets/Scripts/TopDownCharacterMover.cs
@@ -2,17 +2,99 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(InputHandler))]
 public class TopDownCharacterMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private InputHandler _input;
+
+    [SerializeField]
+    private bool RotateTowardMouse;
+
+    [SerializeField]
+    private float MovementSpeed;
+    [SerializeField]
+    private float RotationSpeed;
+
+    [SerializeField]
+    private Camera Camera;
+
+    [SerializeField] GameObject projectile;
+    [SerializeField] Transform shootingPos;
+    [SerializeField] float projectileSpeed = 15f;
+    [SerializeField] float shootingTimer = 0.125f;
+    bool shotFired = false;
+    
+    private void Awake()
     {
-        
+        _input = GetComponent<InputHandler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+        var movementVector = MoveTowardTarget(targetVector);
+
+        if (!RotateTowardMouse)
+        {
+            RotateTowardMovementVector(movementVector);
+        }
+        if (RotateTowardMouse)
+        {
+            RotateFromMouseVector();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            ShootProjectile();
+        }
+
+    }
+
+    void ShootProjectile()
+    {
+        if(!shotFired)
+            StartCoroutine(TimeBetweenShots());
+    }
+
+    private void RotateFromMouseVector()
+    {
+        Ray ray = Camera.ScreenPointToRay(_input.MousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        {
+            var target = hitInfo.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
+    }
+
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = MovementSpeed * Time.deltaTime;
+        // transform.Translate(targetVector * (MovementSpeed * Time.deltaTime)); Demonstrate why this doesn't work
+        //transform.Translate(targetVector * (MovementSpeed * Time.deltaTime), Camera.gameObject.transform);
+
+        targetVector = Quaternion.Euler(0, Camera.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
+        var targetPosition = transform.position + targetVector * speed;
+        transform.position = targetPosition;
+        return targetVector;
+    }
+
+    private void RotateTowardMovementVector(Vector3 movementDirection)
+    {
+        if (movementDirection.magnitude == 0) { return; }
+        var rotation = Quaternion.LookRotation(movementDirection);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, RotationSpeed);
+    }
+
+    IEnumerator TimeBetweenShots()
+    {
+        shotFired = true;
+        var shot = Instantiate(projectile, shootingPos.position, Quaternion.identity);
+        shot.GetComponent<Rigidbody>().AddRelativeForce(shootingPos.up * projectileSpeed, ForceMode.Impulse);
+        yield return new WaitForSeconds(shootingTimer);
+        shotFired = false;
     }
 }
